0ec0f298621b3edf63d1c7a508d807b986df17c9
Miguel Díaz Lozano <x5981md@redsys.es>
2018-02-22 13:48:01 +0100

    Preparada lectura de datos para multivariante.

--- tests/pipeline.py
+++ tests/pipeline.py
@@ -1,16 +1,20 @@
 # -*- coding: utf8 -*-
 
 import sys
-sys.path.append('/Users/migueldiazlozano/Desktop/Ingeniería Informática/TFG/TSF/tsf')
-sys.path.append('/Users/migueldiazlozano/Desktop/Ingeniería Informática/TFG/TSF/tsf/pipeline')
+sys.path.append('/Users/x5981md/time-series-forecasting/tsf')
+sys.path.append('/Users/x5981md/time-series-forecasting/tsf/pipeline')
+sys.path.append('/Users/x5981md/time-series-forecasting/tsf/grid_search')
 from time_series_forescaster import SimpleAR, DinamicWindow, RangeWindow
 from tsf_pipeline import TSFPipeline
+from tsf_grid import TSFGridSearchCV
 from sklearn.linear_model import LassoCV
 from sklearn.metrics import mean_squared_error
+
 
 import click
 import numpy as np
 import pandas as pd
+
 
 # Some stat handlers examples
 def var_function(samples):
@@ -22,24 +26,24 @@ def mean_function(samples):
 
 
 @click.command()
-@click.option('--file', '-f', default=None, required=True, help=u'Fichero de serie temporal')
+@click.option('--files', '-f', type=click.Choice(['temp.txt', 'humidity.txt', 'windDir.txt', 'windSpeed.txt',
+                                                 'QNH.txt']), multiple=True)
 @click.option('--ratio', '-v', default=0.1, required=False, help=u'Ratio de stat total')
 @click.option('--test', '-t', default=0.3, required=False, help=u'Ratio de muestras para test')
-def run_pipeline_test(file, ratio, test):
+def run_pipeline_test(files, ratio, test):
+
     # Read
-    data = pd.read_csv(file, header=None)
-    data = data.values.reshape(1, data.values.shape[0])
-    data = data[0]
+    data = read_data(files)
 
     # Split
-    n_data = len(data)
-    n_test = int(n_data * test)
-    test = data[n_test:]
-    train = data[:n_test]
+    n_data = len(data[0])
+    n_train = int(n_data * (1.0-test))
+    train = data[:, :n_train]
+    test = data[:, n_train:]
 
     # Create pipeline
-    pipe = TSFPipeline([('ar', SimpleAR(n_prev=3)),
-                         ('dw', DinamicWindow(stat=var_function, ratio=ratio, metrics=['variance', mean_function])),
+    pipe = TSFPipeline([('ar', SimpleAR(n_prev=5)),
+                        ('dw', DinamicWindow(ratio=ratio)),
                         ('regressor', LassoCV(random_state=0))])
 
     # Fit pipeline
@@ -56,6 +60,20 @@ def run_pipeline_test(file, ratio, test):
     print "MSE train: " + str(mse_train)
     print "MSE test: " + str(mse_test)
 
+
+def read_data(files):
+    data = []
+    for file in files:
+        path = '../data/' + file
+        single_serie = pd.read_csv(path, header=None).values
+        single_serie = single_serie.reshape(1, single_serie.shape[0])
+
+        if len(data) == 0:
+            data = single_serie
+        else:
+            data = np.append(data, single_serie, axis=0)
+
+    return data
 
 if __name__ == "__main__":
     run_pipeline_test()
