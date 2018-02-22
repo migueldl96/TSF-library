fc11b049e02064af45bac41d7cc5a4593fd29ad2
Miguel Díaz Lozano <x5981md@redsys.es>
2018-02-22 13:04:55 +0100

    Completado script arima

--- arima/arima.py
+++ arima/arima.py
@@ -1,3 +1,5 @@
+# -*- coding: utf8 -*-
+
 import pandas as pd
 import click
 import datetime
@@ -5,37 +7,46 @@ from statsmodels.tsa.arima_model import *
 from sklearn.metrics import mean_squared_error
 
 @click.command()
-@click.option('--data', '-d', default=None, required=True, help=u'Característica a usar:'
-                                                                u'1 - Temperatura'
-                                                                u'2 - ')
+@click.option('--data', '-d', default=1, required=True, help=u'Característica a usar:'
+                                                                u'1 - Velocidad del viento'
+                                                                u'2 - Dirección del viento'
+                                                                u'3 - Temperatura'
+                                                                u'4 - Humedad'
+                                                                u'5 - QNH')
 @click.option('--n_prev', '-n', default=4, required=False, help=u'Muestras previas')
 @click.option('--test', '-t', default=0.3, required=False, help=u'Ratio de muestras para test')
-def run_arima(file, n_prev, test):
+def run_arima(data, n_prev, test):
     def parser(a, b, c, d):
         return datetime.strptime('-'.join([a, b, c, d]), '%Y-%m-%d-%H')
 
     # Read
-    df = pd.read_csv(file, header=None, date_parser=parser, parse_dates={'datetime': [0, 1, 2, 3]}, sep='\t')
-    data = df.values[:, 1]
-    print data
-    quit()
+    df = pd.read_csv('../data/niebla.txt', header=None, date_parser=parser, parse_dates={'datetime': [0, 1, 2, 3]}, sep='\t')
+    data = df.values[:, data]
 
     # Split
     n_data = len(data)
     n_test = int(n_data * test)
-    test = data[n_test:]
-    train = data[:n_test]
+    test, train = data[n_test:], data[:n_test]
 
     # Model
-    arima = ARIMA(train, (n_prev, 0, 0))
+    train_data = [x for x in train]
+    predictions = list()
 
-    fitted_model = arima.fit(disp=0)
+    # walk-forward validation
+    for t in range(len(test)):
+        # fit model
+        model = ARIMA(train_data, order=(n_prev,1,0))
+        model_fit = model.fit(disp=False)
+        # one step forecast
+        yhat = model_fit.forecast()[0]
+        # store forecast and ob
+        predictions.append(yhat)
+        train_data.append(test[t])
 
-    predicted = fitted_model.predict(start=0, end=len(data))
-    print data
-    print predicted
-    # MSE
-    print mean_squared_error(data, predicted[:-1])
+    # evaluate forecasts
+    mse = mean_squared_error(test, predictions)
+
+    print('Test MSE: %.3f' % mse)
 
 
 if __name__ == "__main__":
