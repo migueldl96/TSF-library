33ab5b7df75e81d5ad21798a0471244bc7d365fe
Miguel Díaz Lozano <migueldialoz@gmail.com>
2018-04-17 09:42:10 +0200

    Modificado paquete para contemplar subdirectorios.

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='tsf',
       version='0.1',
@@ -7,6 +7,6 @@ setup(name='tsf',
       author='Miguel Diaz',
       author_email='migueldialoz@gmail.com',
       license='Universidad de Cordoba',
-      packages=['tsf'],
+      packages=find_packages(),
       install_requires=['sklearn', 'numpy'],
       zip_safe=False)
